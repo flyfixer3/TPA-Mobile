8066bf63e4069753ba4e0c6fe42ba2fcf4f5335d
devialiany <devialiany.dl@gmail.com>
2020-10-05 21:59:39 +0700

    2

---
+++ app/src/main/java/com/example/myapplication/RegisterActivtity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivtity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_activtity)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
